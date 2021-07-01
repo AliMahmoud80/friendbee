19e42ba3cef6c11399595ca18bc43276c86d1e38
AliMahmoud80 <ali.mahmoud7080@gmail.com>
2021-07-01 23:17:42 +0200

    first commit

---
+++ js/custom.js
@@ -0,0 +1,29 @@
+﻿
+$(document).ready(function () {
+    $(".navbar-toggler").click(function () {
+        $("header").toggleClass("header-burger");
+    });
+
+    var selectedCountry = $("#selectedCountry").val();
+    $("#countryList option[value=" + selectedCountry + "]").attr("selected", true);
+
+    // $.gdprcookie.init({
+    //     title: 'Cookies & Privacy',
+    //     message: dictionary.cookieConsentMessage,
+    //     acceptBtnLabel: 'Accept all cookies',
+    //     advancedBtnLabel: 'Customise cookies',
+    //     essentialBtnLabel: 'Accept essential cookies',
+    //     cookieTypes: [
+    //         {
+    //             type: 'Essential',
+    //             value: 'essential',
+    //             checked: true
+    //         },
+    //         {
+    //             type: 'Analytics',
+    //             value: 'analytics',
+    //             checked: false
+    //         }]
+    // });
+    $('.collapse').collapse()
+});
