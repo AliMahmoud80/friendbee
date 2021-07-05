9506a4e8e78e955d012ac19c61592f4e0f941a8a
AliMahmoud80 <ali.mahmoud7080@gmail.com>
2021-07-05 14:07:13 +0200

    sixth commit

--- js/custom.js
+++ js/custom.js
@@ -6,25 +6,6 @@
     var selectedCountry = $("#selectedCountry").val();
     $("#countryList option[value=" + selectedCountry + "]").attr("selected", true);
 
-    // $.gdprcookie.init({
-    //     title: 'Cookies & Privacy',
-    //     message: dictionary.cookieConsentMessage,
-    //     acceptBtnLabel: 'Accept all cookies',
-    //     advancedBtnLabel: 'Customise cookies',
-    //     essentialBtnLabel: 'Accept essential cookies',
-    //     cookieTypes: [
-    //         {
-    //             type: 'Essential',
-    //             value: 'essential',
-    //             checked: true
-    //         },
-    //         {
-    //             type: 'Analytics',
-    //             value: 'analytics',
-    //             checked: false
-    //         }]
-    // });
-
     $('.collapse').collapse()
 
     let pathHistory = []
